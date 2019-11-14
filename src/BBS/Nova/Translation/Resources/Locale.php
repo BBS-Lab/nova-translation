43cc3763bea96d641f9fd8a38e1cafc169d88cc0
Sébastien Muller <smuller@big-boss-studio.com>
2019-11-14 14:05:35 +0100

    :sparkles: Add Nova Locale resource

---
+++ src/BBS/Nova/Translation/Resources/Locale.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace BBS\Nova\Translation\Resources;
+
+use App\Nova\Resource;
+use BBS\Nova\Translation\Models\Locale as Model;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+
+class Locale extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'BBS\\Nova\\Translation\\Models\\Locale';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static $title = 'label';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static $search = [
+        'iso',
+        'label',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public static $group = 'Other';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Text::make('ISO')
+                ->sortable()
+                ->rules('required', 'max:255')
+                ->creationRules('unique:locales,iso')
+                ->updateRules('unique:locales,iso,{{resourceId}}'),
+
+            Text::make('Label')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Select::make('Fallback', 'fallback_id')
+                ->options(Model::query()->select('id', 'label')->orderBy('label', 'asc')->get()->pluck('label', 'id')->toArray())
+                ->nullable()
+                ->hideFromIndex()
+                ->displayUsingLabels(),
+
+            Boolean::make('Is available in API?', 'available_in_api'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
