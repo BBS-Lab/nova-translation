ebfb325a93784f31242fdd63c8a9406a1f3355e0
Sébastien Muller <smuller@big-boss-studio.com>
2019-12-03 11:43:28 +0100

    :sparkles: Setup StoreController

--- src/Http/Controllers/TranslatableResource/StoreController.php
+++ src/Http/Controllers/TranslatableResource/StoreController.php
@@ -2,9 +2,12 @@
 
 namespace BBSLab\NovaTranslation\Http\Controllers\TranslatableResource;
 
+use BBSLab\NovaTranslation\Models\Locale;
 use BBSLab\NovaTranslation\Resources\TranslatableResource;
 use Illuminate\Support\Facades\DB;
 use Laravel\Nova\Actions\ActionEvent;
+use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Controllers\ResourceStoreController;
 use Laravel\Nova\Http\Requests\CreateResourceRequest;
 
@@ -15,44 +18,108 @@ class StoreController extends ResourceStoreController
      */
     public function handle(CreateResourceRequest $request)
     {
-        echo 'YO'; exit;
         $resource = $request->resource();
-        if ($resource instanceof TranslatableResource) {
-            dd($resource, $request);
+
+        if (is_subclass_of($resource, TranslatableResource::class)) {
+            $resource::authorizeToCreate($request);
+            $resource::validateForCreation($request);
+
+            $model = DB::transaction(function () use ($request, $resource) {
+                $models = $this->storeTranslatable($request, $resource);
+
+                /** @var \BBSLab\NovaTranslation\Models\Locale $currentLocale */
+                $currentLocale = Locale::query()->select('id')->where('iso', '=', app()->getLocale())->first();
+
+                return $models[$currentLocale->id];
+            });
+
+            return response()->json([
+                'id' => $model->getKey(),
+                'resource' => $model->attributesToArray(),
+                'redirect' => $resource::redirectAfterCreate($request, $request->newResourceWith($model)),
+            ], 201);
         } else {
             return parent::handle($request);
         }
+    }
 
-        /*
-        $resource::authorizeToCreate($request);
+    /**
+     * Store translatable.
+     *
+     * @param  \Laravel\Nova\Http\Requests\CreateResourceRequest  $request
+     * @param  string  $resource
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    protected function storeTranslatable(CreateResourceRequest $request, string $resource)
+    {
+        $model = $resource::newModel();
 
-        $resource::validateForCreation($request);
+        $creationFields = (new $resource($model))->creationFields($request);
+        $translatedData = $this->mapTranslatableData($request, $creationFields);
 
-        $model = DB::transaction(function () use ($request, $resource) {
-            [$model, $callbacks] = $resource::fill(
-                $request, $resource::newModel()
-            );
+        $translatedModels = [];
+        foreach ($translatedData as $localeId => $fields) {
+            $translationId = $fields['translation_id'];
+            unset($fields['translation_id']);
 
-            if ($request->viaRelationship()) {
-                $request->findParentModelOrFail()
-                    ->{$request->viaRelationship}()
-                    ->save($model);
-            } else {
-                $model->save();
+            $translatedModels[$localeId] = $resource::newModel();
+            foreach ($fields as $field => $value) {
+                $translatedModels[$localeId]->$field = $value;
             }
 
-            ActionEvent::forResourceCreate($request->user(), $model)->save();
+            if ($request->viaRelationship()) {
+                $request->findParentModelOrFail()->{$request->viaRelationship}()->save($translatedModels[$localeId]);
+                // @TODO... Check for Translation entry for new created model
+            } else {
+                $translatedModels[$localeId]->save();
+                $translatedModels[$localeId]->upsertTranslationEntry($localeId, $translationId);
+            }
 
-            collect($callbacks)->each->__invoke();
+            ActionEvent::forResourceCreate($request->user(), $translatedModels[$localeId])->save();
 
-            return $model;
-        });
+            // @TODO...
+            // collect($callbacks)->each->__invoke();
+        }
 
-        return response()->json([
-            'id' => $model->getKey(),
-            'resource' => $model->attributesToArray(),
-            'redirect' => $resource::redirectAfterCreate($request, $request->newResourceWith($model)),
-        ], 201);
-        */
+        return $translatedModels;
+    }
+
+    /**
+     * Map translatable data.
+     *
+     * @param  \Laravel\Nova\Http\Requests\CreateResourceRequest  $request
+     * @param  \Laravel\Nova\Fields\FieldCollection  $fields
+     * @return array
+     */
+    protected function mapTranslatableData(CreateResourceRequest $request, FieldCollection $fields)
+    {
+        $translatedData = [];
+
+        foreach ($fields as $panel) {
+            foreach ($panel['fields'] as $field) {
+                /** @var \Laravel\Nova\Fields\Field $field */
+                $localeId = $field->meta()['localeId'];
+                if (! isset($translatedData[$localeId])) {
+                    $translatedData[$localeId] = [];
+                }
+
+                $attribute = $this->translatableAttribute($field, $localeId);
+                $translatedData[$localeId][$attribute] = $request->input($attribute.'.'.$localeId);
+            }
+        }
+
+        return $translatedData;
+    }
+
+    /**
+     * Return translatable attribute name.
+     *
+     * @param  \Laravel\Nova\Fields\Field  $field
+     * @param  int  $localeId
+     * @return string
+     */
+    protected function translatableAttribute(Field $field, int $localeId)
+    {
+        return substr($field->attribute, 0, strpos($field->attribute, '['.$localeId.']'));
     }
 }
