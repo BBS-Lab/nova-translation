8420d50c50bfb2763fa376c316236cfb16bc0f30
Sébastien Muller <smuller@big-boss-studio.com>
2019-12-04 18:26:48 +0100

    :recycle: Refactor LocaleFilters trait

--- src/GraphQL/Directives/Traits/LocaleFilters.php
+++ src/GraphQL/Directives/Traits/LocaleFilters.php
@@ -2,18 +2,21 @@
 
 namespace BBSLab\NovaTranslation\GraphQL\Directives\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
 trait LocaleFilters
 {
     /**
-     * Filter model by locale filters.
+     * Get ISOS from GraphQL args.
      *
-     * @param  string  $modelClass
      * @param  array  $args
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @return array
      */
-    protected function localeFilters(string $modelClass, array $args)
+    public function isosFromLocaleFilters(array $args)
     {
         $isos = [];
+
         if (isset($args['localeFilters'])) {
             if (isset($args['localeFilters']['locale'])) {
                 $isos[] = $args['localeFilters']['locale'];
@@ -23,15 +26,26 @@ trait LocaleFilters
             }
         }
 
-        /** @var \Illuminate\Database\Eloquent\Model $model */
-        $model = new $modelClass;
+        return $isos;
+    }
+
+    /**
+     * Filter locales query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  array  $isos
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function filterLocalesQuery(Builder $query, Model $model, array $isos = [])
+    {
         $table = $model->getTable();
 
-        $query = $modelClass::query()
+        $query = $query
             ->select($table.'.*', 'locales.iso AS locale', 'translations.translation_id')
             ->join('translations', $table.'.'.$model->getKeyName(), '=', 'translations.translatable_id')
             ->join('locales', 'translations.locale_id', '=', 'locales.id')
-            ->where('translations.translatable_type', '=', $modelClass)
+            ->where('translations.translatable_type', '=', get_class($model))
             ->where('locales.available_in_api', '=', true);
 
         if (! empty($isos)) {
@@ -39,5 +53,24 @@ trait LocaleFilters
         }
 
         return $query;
+    }
+
+    /**
+     * Filter model by locale filters.
+     *
+     * @param  string  $modelClass
+     * @param  array  $args
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function localeFilters(string $modelClass, array $args)
+    {
+        /** @var \Illuminate\Database\Eloquent\Model $model */
+        $model = new $modelClass;
+
+        return $this->filterLocalesQuery(
+            $modelClass::query(),
+            $model,
+            $this->isosFromLocaleFilters($args)
+        );
     }
 }
