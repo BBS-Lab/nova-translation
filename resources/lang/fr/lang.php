78147e53708241e6c10d1e321e8d5a89b5cebf61
Sébastien Muller <smuller@big-boss-studio.com>
2019-12-06 11:09:50 +0100

    :globe_with_meridians: Add FR lang files

---
+++ resources/lang/fr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'menu' => 'Traductions',
+
+    'field' => 'Traduction',
+
+    'labels' => [
+        'types' => [
+            'text' => 'Texte',
+            'upload' => 'Upload',
+        ],
+    ],
+
+];
