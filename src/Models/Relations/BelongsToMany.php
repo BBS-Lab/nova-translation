d8c1b3a792c868ed4a75b0abfdd429200330e5bd
Mikaël Popowicz <mpopowicz@big-boss-studio.com>
2022-11-28 14:43:35 +0100

    🐛 Fix BelongsToMany.

--- src/Models/Relations/BelongsToMany.php
+++ src/Models/Relations/BelongsToMany.php
@@ -31,7 +31,7 @@ class BelongsToMany extends Relation
     {
         parent::attach($id, $attributes, $touch);
 
-        if (! in_array($parent->getMorphClass(), nova_translation()->translatableModels())) {
+        if (! in_array($this->parent->getMorphClass(), nova_translation()->translatableModels())) {
             return;
         }
 
@@ -59,7 +59,7 @@ class BelongsToMany extends Relation
     {
         $result = parent::detach($ids, $touch);
 
-        if (! in_array($parent->getMorphClass(), nova_translation()->translatableModels())) {
+        if (! in_array($this->parent->getMorphClass(), nova_translation()->translatableModels())) {
             return $result;
         }
 
